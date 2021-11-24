011e052f8b70c7cae359257d381f88b547a5ef12
sam <adming@minemintteam.app>
2021-11-24 01:58:22 -0500

    Final Touch

--- src/Server.cs
+++ src/Server.cs
@@ -19,6 +19,7 @@ namespace GameServer
             Console.WriteLine("Client connected");
 
             gameThread = new Thread(new ThreadStart(Run));
+            gameThread.Start();
         }
 
         private void Run()
@@ -26,18 +27,20 @@ namespace GameServer
             while (true)
             {
                 byte[] buffer = new byte[1024];
-                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                int? bytesRead = stream?.Read(buffer, 0, buffer.Length);
                 if (bytesRead == 0)
                 {
                     Console.WriteLine("Client disconnected");
                     break;
                 }
-                string data = System.Text.Encoding.ASCII.GetString(buffer, 0, bytesRead);
-                Console.WriteLine(data);
-                string[] splitData = data.Split('|');
+                string data = System.Text.Encoding.ASCII.GetString(buffer, 0, bytesRead.HasValue ? bytesRead.Value : 0);
+                string[] splitData = data.Split('\r');
                 switch (splitData[0])
                 {
                     case "LOGIN":
+                        break;
+                    case "GET / HTTP/1.1":
+                        Console.WriteLine("Hit GET");
                         break;
                 }
             }
